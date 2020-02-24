0fd8718e0358abb2aa0fab26bc0606808e2c9019
tyson boren <tyson.d.boren@gmail.com>
2020-02-24 15:23:31 -0700

    added a ternary operator nav container button

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import PortfolioContainer from "./portfolio/portfolio-container"
-import portfolioItem from './portfolio/portfolio-item';
+import NavigationComponent from "./navigation/navigation-container"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+      <NavigationComponent />
         <h1>Tyson Boren Portfolio</h1>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
