b5548a2bcee1865b0ec3f576456beb08ac636343
tyson boren <tyson.d.boren@gmail.com>
2020-02-26 15:36:49 -0700

    class video title: Updatign styles with react event listeners.

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,35 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 
 
 
 
-export default function(props) {
-    //data we need:
-        //bakcground image:thumb_image_url
-        //logo: logo_url
-        //description: description
-        //id: id
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props)
 
-        const{ id, description, thumb_image_url, logo_url } = props.item
-    return (
-        <div className="portfolio-item-wrapper">
-            <div
-                className='portfolio-img-background'
-                style={{
-                    // backgroundImage: "url(" + thumb_image_url + ")",
-                    backgroundImage: `url(${thumb_image_url})`
-                }}
-            />
+        this.state = {
+            portfolioItemClass: ""
+        }
+        //binds for below funcitons IF you dont use arrow functions. 
+        // this.onMouseEnter = this.onMouseEnter.bind(this)
+        // this.onMouseLeave = this.onMouseLeave.bind(this)
+    }
 
-            <div className="img-text-wrapper">
-                <div className="logo-wrapper">
-                    <img src={logo_url} />
+    handleMouseEnter() {
+        this.setState({portfolioItemClass: "image-blur"})
+    }
+
+    handleMouseLeave() {
+        this.setState({portfolioItemClass: ""})
+    }
+
+    render() {
+        const{ id, description, thumb_image_url, logo_url } = this.props.item
+            //data we need:
+            //bakcground image:thumb_image_url
+            //logo: logo_url
+            //description: description
+            //id: id
+
+        return (
+            <div className="portfolio-item-wrapper"
+            //these are the functions you need to bind UNLESS you use arrow functions. 
+            // onMouseEnter={this.handleMouseEnter}
+            // onMouseLeave={this.handleMouseLeave}
+            onMouseEnter={() => this.handleMouseEnter()}
+            onMouseLeave={() => this.handleMouseLeave()}
+            
+            >
+                <div
+                    className={'portfolio-img-background ' + this.state.portfolioItemClass}
+                    style={{
+                        // backgroundImage: "url(" + thumb_image_url + ")",
+                        backgroundImage: `url(${thumb_image_url})`
+                    }}
+                />
+
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
+
+                    <div className="subtitle">{description}</div>
                 </div>
-
-                <div className="subtitle">{description}</div>
             </div>
-        </div>
-    )
+        )
+    }
 }
