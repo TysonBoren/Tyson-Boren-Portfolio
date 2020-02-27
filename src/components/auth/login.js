9122314caea564775345d758835866a631a481d6
tyson boren <tyson.d.boren@gmail.com>
2020-02-27 13:30:09 -0700

    class video title: introduction to react form. added submit and change handlers for our input forms

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: "",
+            password: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange() {
+        console.log("handle change", event)
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+
     }
 
+    handleSubmit(event) {
+        console.log('handle submit', event)
+        this.setState({
 
+        })
+    }
 
 
 
@@ -13,10 +32,29 @@ export default class Login extends Component {
         return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+                
+                <h2>{this.state.email}</h2>
+                <h2>{this.state.password}</h2>
 
-                <form>
-                    <input type="text"/>
-                    <input type="password"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="Your email..."
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Your Password..."
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
                 </form>
 
 
