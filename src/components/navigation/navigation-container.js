53501def9f6016b8ecd73a9dd42e385301e408f5
tyson boren <tyson.d.boren@gmail.com>
2020-02-28 15:17:34 -0700

    finalizing the logout functionality with a higher order component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from "axios";
+import { withRouter } from "react-router";
 import { NavLink } from "react-router-dom"
 
 const NavigationComponent = (props) => {
@@ -9,8 +11,28 @@ const NavigationComponent = (props) => {
                     Blog
                 </NavLink>
             </div>
-        )
+        );
+    };
+
+    const handleSignOut = () => {
+        axios.delete("https://api.devcamp.space/logout", 
+            { withCredentials: true 
+            })
+            .then(response => { 
+            if (response.status === 200) {
+                props.history.push("/");
+                props.handleSuccessfulLogout();
+            }
+            return response.data;
+        })
+        .catch(error => {
+            console.log("Error signing out", error)
+        });
     }
+
+
+
+
     return (
         <div className='nav-wrapper'>
             <div className="left-side">
@@ -39,9 +61,11 @@ const NavigationComponent = (props) => {
 
             <div className="right-side">
                 Tyson Boren
+                {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null } 
+
             </div>
         </div>
     )
 }
 
-export default NavigationComponent
+export default withRouter(NavigationComponent);
