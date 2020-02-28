9a40de030163afe766b0adc1dc5fef62b3a20ccc
tyson boren <tyson.d.boren@gmail.com>
2020-02-28 14:25:34 -0700

    building a route guard in react

--- src/components/app.js
+++ src/components/app.js
@@ -73,6 +73,12 @@ export default class App extends Component {
     this.checkLoginStatus();
   }
 
+  authorizedPages() {
+    return [
+      <Route path="/blog" component={Blog} />
+    ]
+  }
+
 
 
   render() {
@@ -90,7 +96,10 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
 
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              
+              { this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null }
+
+
               <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
               <Route path='/auth' render={props => (
                 <Auth 
