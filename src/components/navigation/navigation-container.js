e5b962dabad8cf9af335a790719f95521bcca692
tyson boren <tyson.d.boren@gmail.com>
2020-02-28 13:58:29 -0700

    how to convert a class component into a functional component in react

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from "react-router-dom"
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super()
-    }
+const NavigationComponent = (props) => {
+    
 
+    return (
+        <div className='nav-wrapper'>
+            <div className="left-side">
 
-
-    render() {
-        return (
-            <div className='nav-wrapper'>
-                <div className="left-side">
-
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/" activeClassName="nav-link-active">
-                            Home
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/about-me" activeClassName="nav-link-active">
-                            About
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/Contact" activeClassName="nav-link-active">
-                            Contact
-                        </NavLink>
-                    </div>
-
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/Blog" activeClassName="nav-link-active">
-                            Blog
-                        </NavLink>
-                    </div>
-                        
+                <div className="nav-link-wrapper">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
                 </div>
 
-                <div className="right-side">
-                    Tyson Boren
+                <div className="nav-link-wrapper">
+                    <NavLink to="/about-me" activeClassName="nav-link-active">
+                        About
+                    </NavLink>
                 </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/Contact" activeClassName="nav-link-active">
+                        Contact
+                    </NavLink>
+                </div>
+
+                <div className="nav-link-wrapper">
+                    <NavLink to="/Blog" activeClassName="nav-link-active">
+                        Blog
+                    </NavLink>
+                </div>
+                    
             </div>
-        )
-    }
+
+            <div className="right-side">
+                Tyson Boren
+            </div>
+        </div>
+    )
 }
+
+export default NavigationComponent
