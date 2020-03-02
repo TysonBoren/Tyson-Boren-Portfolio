b8f67cae0734b1c3e45217957bcb43552157b8c5
tyson boren <tyson.d.boren@gmail.com>
2020-03-02 13:26:25 -0700

    Fixing Multiple Key Prop Warnings in React

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -19,19 +19,28 @@ export default class PortfolioForm extends Component {
     }
 
 
+    buildForm() {
+        let formData = new FormData();
 
+        formData.append("portfolio_item[name]", this.state.name);
+        formData.append("portfolio_item[description]", this.state.description);
+        formData.append("portfolio_item[url]", this.state.url);
+        formData.append("portfolio_item[category]", this.state.category);
+        formData.append("portfolio_item[position]", this.state.position);
+
+        return formData;
+    }
 
 
     handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value
-            [event.preventDefault()]
         });
     }
 
     handleSubmit(event) {
-        console.log("event", event)
-        
+        this.buildForm();
+            event.preventDefault();
     }
 
 
