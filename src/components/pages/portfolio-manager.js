c8aab068e30b05cbb5eda0a25089a87d338f6922
tyson boren <tyson.d.boren@gmail.com>
2020-03-02 11:12:30 -0700

    Creating the Portfolio Form and Calling it from the Parent Component

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list'
+import PortfolioForm from '../portfolio/portfolio-form';
+
 
 export default class PortfolioManager extends Component {
     constructor() {
@@ -9,6 +11,19 @@ export default class PortfolioManager extends Component {
         this.state={
             portfolioItems: []
         }
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
+        this.handleFromSubmissionError = this.handleFromSubmissionError.bind(this)
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        // TODO
+        // update the portfolioItems state
+        // and add the portfolioItem to the list
+    }
+
+    handleFromSubmissionError(error) {
+        console.logt("handleFormSubmissionError", error)
     }
 
     getPortfolioItems() {
@@ -34,8 +49,10 @@ export default class PortfolioManager extends Component {
         return (
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
-                <h1>PortfolioManager</h1>
-
+                    <PortfolioForm 
+                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleFromSubmissionError={this.handleFromSubmissionError}
+                    />
                 </div>
 
                 <div className="right-column">
---
+++ src/components/portfolio/portfolio-form.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+export default class PortfolioForm extends Component {
+    constructor() {
+        super()
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>PortfolioForm</h1>
+                <h1>PortfolioForm</h1>
+            </div>
+        )
+    }
+}
