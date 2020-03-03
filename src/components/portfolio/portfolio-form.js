fb573b8b57eabfa7837904942d591fb30664b7f8
tyson boren <tyson.d.boren@gmail.com>
2020-03-03 10:35:34 -0700

    Creating a Form Input Mixin

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -127,10 +127,7 @@ export default class PortfolioForm extends Component {
 
     render() {
         return (
-            <div>
-                <h1>PortfolioForm</h1>
-
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
                     <div>
                         <input
                             type="text"
@@ -210,7 +207,6 @@ export default class PortfolioForm extends Component {
 
                     
                 </form>
-            </div>
         )
     }
 }
