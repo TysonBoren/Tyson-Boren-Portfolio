e2b8ca8a1ece871f0e87b95e3ae0483ed2aefc25
tyson boren <tyson.d.boren@gmail.com>
2020-03-03 13:19:01 -0700

    Building a Delete Click Handler in React

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -14,6 +14,11 @@ export default class PortfolioManager extends Component {
 
         this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
         this.handleFromSubmissionError = this.handleFromSubmissionError.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this)
+    }
+
+    handleDeleteClick(portfolioItem) {
+        console.log("handleDeleteClick", portfolioItem)
     }
 
     handleSuccessfulFormSubmission(portfolioItem) {
@@ -58,7 +63,11 @@ export default class PortfolioManager extends Component {
                 </div>
 
                 <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems} />
+                    <PortfolioSidebarList 
+                        handleDeleteClick={this.handleDeleteClick} 
+                        data={this.state.portfolioItems} 
+
+                />
 
                 </div>
             </div>
--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -11,6 +11,9 @@ const PortfolioSidebarList = props => {
 
                 <h1 className="title">{portfolioItem.name}</h1>
                 <h2>{portfolioItem.id}</h2>
+                <a onClick={() => props.handleDeleteClick(portfolioItem)}>
+                    Delete
+                </a>
             </div>
         )
     })
