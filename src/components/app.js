149bbc1d6c403684f8a761b927ecd16c383c9534
tyson boren <tyson.d.boren@gmail.com>
2020-03-06 14:59:07 -0700

    Implementing an Animated Data Loading Icon in React

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FortAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt, faEdit, faDharmachakra} from "@fortawesome/free-solid-svg-icons";
 
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
@@ -18,7 +18,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from './pages/no-match';
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faDharmachakra);
 
 
 export default class App extends Component {
