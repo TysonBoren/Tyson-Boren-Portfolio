f204d3d80949dfb3b853c0d0bbb7cc589548293e
tyson boren <tyson.d.boren@gmail.com>
2020-03-06 15:47:02 -0700

    Building the Full Infinite Scroll Functionality in React

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -21,12 +21,13 @@ export default class Blog extends Component {
 
     activateInfiniteScroll() {
         window.onscroll = () => {
-            
-            if (
-                window.innerHeight + document.documentElement.scrollTop === 
-                document.documentElement.offsetHeight
-                ) {
-                console.log("get more posts")
+            if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+                    return;
+                }
+
+            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+            ) {
+                this.getBlogItems();
             }
         }
     }
@@ -36,11 +37,13 @@ export default class Blog extends Component {
         this.setState({
             currentPage: this.state.currentPage + 1
         })
-        axios.get("https://tysonboren.devcamp.space/portfolio/portfolio_blogs", { 
+        axios.get(`https://tysonboren.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, { 
             withCredentials: true 
         }).then(response => {
+            console.log("getting", response.data)
             this.setState({
-                blogItems: response.data.portfolio_blogs,
+                blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+                // blogItems: response.data.portfolio_blogs,
                 totalCount: response.data.meta.total_records,
                 isLoading: false
             })
@@ -78,6 +81,8 @@ export default class Blog extends Component {
                     null
                 )}
             </div>
+
+            //this would work on pages w/o infinite scroll
             /* <div className='blog-container'>
                 {this.state.isLoading ? (
                 <div className='content-loader'> 
