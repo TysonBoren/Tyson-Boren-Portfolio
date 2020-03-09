ef8bca1c0d674395dd1cb78fa62a3f6699694bcb
tyson boren <tyson.d.boren@gmail.com>
2020-03-09 15:45:27 -0600

    Rendering Draft JS Form Component and Building Rich Text Editor Styles

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import RichTextEditor from "../forms/rich-text-editor"
 
 
 
@@ -70,6 +71,10 @@ export default class BlogForm extends Component {
                     placeholder="Blog status"
                     value={this.state.blog_status}
                 />
+            </div>
+
+            <div className="one-column">
+                <RichTextEditor />
             </div>
 
                 <button className = "btn">Save</button>
