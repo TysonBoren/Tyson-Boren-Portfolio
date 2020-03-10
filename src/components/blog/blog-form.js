d4d6a350de16df6e868104ca6e5a0b23ec39afa0
tyson boren <tyson.d.boren@gmail.com>
2020-03-10 15:26:36 -0600

    Deleting Featured Blog Images via API and Updating Parent Component State

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -24,8 +24,20 @@ export default class BlogForm extends Component {
         this.componentConfig = this.componentConfig.bind(this)
         this.djsConfig = this.djsConfig.bind(this)
         this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this)
+        this.deleteImage = this.deleteImage.bind(this)
         
         this.featuredImageRef = React.createRef();
+    }
+
+    deleteImage(imageType) {
+        axios.delete(
+            `https://api.devcamp.space/portfolio/delete-portfolio-blog-image/${this.props.blogToEdit.id}?image_type=${imageType}`,
+             { withCredentials: true }
+        ).then(response => {
+            this.props.handleFeaturedImageDelete();
+        }).catch(error => {
+            console.log('deleteImage error', error)
+        })
     }
 
     componentWillMount() {
@@ -152,7 +164,7 @@ export default class BlogForm extends Component {
                         <img src={this.props.blogToEdit.featured_image_url} />
 
                         <div className="image-removal-link">
-                            <a>Remove Banner Image</a>
+                            <a onClick={() => this.deleteImage("featured_image")}>Remove Image</a>
                         </div>
                     </div>
                     ) : (
