0b3855052dfbc861e102435bbdf3b908f396e6d5
tyson boren <tyson.d.boren@gmail.com>
2020-03-11 14:55:30 -0600

    Building Contact Page Styles for the React Portfolio

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function() {
     return (
-        <div>
-            Contact
+        <div className="content-page-wrapper">
+            <div 
+            className="left-column"
+            style={{
+                background: "url(" + contactPagePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+            }}
+            />
+            <div className="right-column">
+                <div className="contact-bullet-point">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon='phone'/>
+                        </div>
+
+                        <div className="text">
+                            555-555-5555
+                        </div>
+                    </div>
+
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon='envelope'/>
+                        </div>
+
+                        <div className="text">
+                            Tyson.D.Boren@gmail.com
+                        </div>
+                    </div>
+
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon='map-marked-alt'/>
+                        </div>
+
+                        <div className="text">
+                            Saratoga Springs, Utah
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,10 +1,28 @@
-import { faTrash, faSignOutAlt, faEdit, faDharmachakra, faPlusCircle} from "@fortawesome/free-solid-svg-icons";
+import { 
+    faTrash, 
+    faSignOutAlt, 
+    faEdit, 
+    faDharmachakra, 
+    faPlusCircle,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
+} from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 
 
 const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faDharmachakra ,faPlusCircle);
+    return library.add(
+        faTrash, 
+        faSignOutAlt, 
+        faEdit, 
+        faDharmachakra, 
+        faPlusCircle, 
+        faPhone, 
+        faEnvelope, 
+        faMapMarkedAlt
+        );
 
 }
 
